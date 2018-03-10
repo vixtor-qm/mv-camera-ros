12a20cea91b708944e44bf5ce28c8a9532bd298c
Victor Sakharchuk <vctrshr@gmail.com>
2018-03-10 11:59:00 +0800

    added MV camera ros binding

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <CameraApi.h>
+
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <camera_info_manager/camera_info_manager.h>
 /*
 void imageDisparityCallback(const sensor_msgs::ImageConstPtr& msgDisp)
 {
@@ -38,51 +44,92 @@ void cameraInfoCallback(const sensor_msgs::CameraInfoConstPtr& camInfo)
 }
 */
 
-unsigned char           * g_pRgbBuffer;  
+unsigned char * g_pRgbBuffer;
 
-int main(int argc, char **argv)
+int main_mv(int argc, char **argv)
 {
-     int                     iCameraCounts = 1;
-    int                     iStatus=-1;
-    tSdkCameraDevInfo       tCameraEnumList;
-    int                     hCamera;
-    tSdkCameraCapbility     tCapability;      //设备描述信息
-    tSdkFrameHead           sFrameInfo;
-    BYTE*                   pbyBuffer;
-    int                     iDisplayFrames = 10000;
-    IplImage *iplImage = NULL;
+  int                     iCameraCounts = 1;
+  int                     iStatus=-1;
+  tSdkCameraDevInfo       tCameraEnumList;
+  int                     hCamera;
+  tSdkCameraCapbility     tCapability;      //设备描述信息
+  tSdkFrameHead           sFrameInfo;
+  BYTE*                   pbyBuffer;
+  int                     iDisplayFrames = 10000;
+  IplImage *              iplImage = NULL;
+  int                     channel=3;
 
-    int status = CameraSdkInit(1);
-    std::cout << "Camera SDK init status : " << status << std::endl;
+  // 0 for English, 1 for Chinese
+  int status = CameraSdkInit(1);
+  std::cout << "Camera SDK init status : " << status << std::endl;
 
 
-    status = CameraEnumerateDevice(&tCameraEnumList,&iCameraCounts);
-    if(iCameraCounts==0)
-    {  
-      std::cout << "No camera found" << std::endl;
-      std::cout << "Status = " << status << std::endl;
-        return -1;
-    }
+  status = CameraEnumerateDevice(&tCameraEnumList,&iCameraCounts);
+  if(iCameraCounts==0)
+  {
+    std::cout << "No camera found" << std::endl;
+    std::cout << "Status = " << status << std::endl;
+    return -1;
+  }
 
-    iStatus = CameraInit(&tCameraEnumList,-1,-1,&hCamera);
+  iStatus = CameraInit(&tCameraEnumList,-1,-1,&hCamera);
 
-    if(iStatus!=CAMERA_STATUS_SUCCESS)
+  if(iStatus!=CAMERA_STATUS_SUCCESS)
+  {
+    return -1;
+  }
+
+  std::cout << "Camera is detected" << std::endl;
+
+  CameraGetCapability(hCamera,&tCapability);
+
+  g_pRgbBuffer = (unsigned char*)malloc(tCapability.sResolutionRange.iHeightMax*tCapability.sResolutionRange.iWidthMax*3);
+
+  CameraPlay(hCamera);
+
+
+  if(tCapability.sIspCapacity.bMonoSensor)
+  {
+    channel=1;
+    CameraSetIspOutFormat(hCamera,CAMERA_MEDIA_TYPE_MONO8);
+  }else
+  {
+    channel=3;
+    CameraSetIspOutFormat(hCamera,CAMERA_MEDIA_TYPE_BGR8);
+  }
+
+
+  ros::init(argc, argv, "mv-camera-node");
+  ros::NodeHandle n;
+  ros::Rate rate(30.0);
+  while (n.ok())
+  {
+    if(CameraGetImageBuffer(hCamera,&sFrameInfo,&pbyBuffer,1000) == CAMERA_STATUS_SUCCESS)
     {
-        return -1;
+      CameraImageProcess(hCamera, pbyBuffer, g_pRgbBuffer, &sFrameInfo);
+
+      cv::Mat image(sFrameInfo.iHeight, sFrameInfo.iWidth, CV_8UC3, g_pRgbBuffer, cv::Mat::AUTO_STEP);
+      /*
+      if (iplImage)
+      {
+        cvReleaseImageHeader(&iplImage);
+      }
+      iplImage = cvCreateImageHeader(cvSize(sFrameInfo.iWidth,sFrameInfo.iHeight),IPL_DEPTH_8U,channel);
+      cvSetData(iplImage,g_pRgbBuffer,sFrameInfo.iWidth*channel);
+      */
+#if 0
+      cvShowImage("OpenCV Demo",iplImage);
+#else
+      cv::imshow("OpenCV Demo", image);
+#endif
+      CameraReleaseImageBuffer(hCamera,pbyBuffer);
+
     }
+    ros::spinOnce();
+    rate.sleep();
+  }
 
-    std::cout << "Camera is detected" << std::endl;
-
-    CameraGetCapability(hCamera,&tCapability);
-
-    ros::init(argc, argv, "mv-camera-node");
-    ros::NodeHandle n;
-    ros::Rate rate(30.0);
-    while (n.ok())
-    {
-        ros::spinOnce();
-        rate.sleep();
-    }
-    //cv::waitKey(0);
-    return 0;
+  CameraUnInit(hCamera);
+  free(g_pRgbBuffer);
+  return 0;
 }
